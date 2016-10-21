beb07b90d7b10f65b54ff727e6ad286c1c1b584a
Brandon <bmduke1997@gmail.com>
2016-10-20 21:21:30 -0400

    new key

--- Connect5/src/GUI.java
+++ Connect5/src/GUI.java
@@ -125,7 +125,7 @@ public class GUI  extends JFrame {
             statusLbl.setFont(new Font("Default", Font.BOLD, 20));
             gameWindow.add(statusLbl, BorderLayout.PAGE_END);
 
-            JLabel background1 = new JLabel(new ImageIcon("src/ing/gar.png"));
+            JLabel background1 = new JLabel(new ImageIcon("src/ing/Connect5Key.png"));
             gameWindow.add(background1, BorderLayout.WEST);
 
             boardPanel.setBackground(Color.CYAN);
