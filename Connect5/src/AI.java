21a23a9e1f2c4a66787093c87f6080d30be0add5
Nick Zarilla <nickzarilla@gmail.com>
2016-10-22 16:57:00 -0400

    wrote heuristic

--- Connect5/src/AI.java
+++ Connect5/src/AI.java
@@ -8,11 +8,13 @@ import java.util.Vector;
 public class AI implements Runnable {
 
 	private int[][][] board;
-	private MoveTree moves = new MoveTree();
+	private MoveTree moves = new MoveTree(0);
 	Thread aiThread = new Thread(this);
+	int playerCount;
 
-	public AI(int[][][] board) {
+	public AI(int[][][] board, int playerCount) {
 		this.board = board; // Get the board.
+		this.playerCount = playerCount;
 	}
 
 	public void start() {
@@ -21,34 +23,62 @@ public class AI implements Runnable {
 
 	public void update(int[][][] board) {
 		aiThread.interrupt(); // Stop the AI...
-		this.board = board;   // and update the board.
+		this.board = board; // and update the board.
 	}
 
 	public void run() {
 		/*
 		 * Basically everything the AI has to do initializes here.
 		 */
-		try {
-			moves.populateAll();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		Vector<Thread> threads;
+		while (true) {
+			threads = new Vector<Thread>();
+			try {
+				moves.populate();
+				for (MoveTree move : moves.possibleMoves) {
+					threads.addElement(new Thread(new Runnable() {
+						public void run() {
+							try {
+								move.populateAll();
+							} catch (InterruptedException e) {
+							}
+						}
+					}));
+				}
+			} catch (InterruptedException e) {
+				for (Thread thread : threads) {
+					thread.interrupt();
+				}
+				try {
+					wait(100);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			for ( Thread thread: threads)
+				try {
+					thread.join();
+				} catch (InterruptedException e) {
+				}
 		}
 	}
 
-	public int[] getMove() {
+	public Integer[] getMove() {
 		/*
 		 * Get the best move from the MoveTree.
 		 */
 		MoveTree best = null;
 		for (MoveTree move : moves.possibleMoves) {
-			if (best == null) best = move;
-			else if (move.value > best.value) best = move;
+			if (best == null)
+				best = move;
+			else if (move.getHeuristic() > best.getHeuristic())
+				best = move;
 		}
 		return best.coords;
 	}
 
-	public int[] getMove(int time) {
+	public Integer[] getMove(int time) {
 		/*
 		 * Wait for awhile before getting the move.
 		 */
@@ -65,73 +95,213 @@ public class AI implements Runnable {
 		/*
 		 * Well, it's a tree of possible moves.
 		 */
-		private int[] coords = new int[3];
-		private int value = 0;
+		private Integer[] coords = new Integer[3];
 		private Vector<MoveTree> possibleMoves;
+		private Double heuristicValue = null;
+		int player;
 
 		public MoveTree getMove(int move) {
 			return possibleMoves.elementAt(move);
 		}
 
-		public MoveTree() {
+		public MoveTree(int player) {
 			/*
-			 * Initialize a MoveTree without coordinates.
-			 * Primarily used for the root of the tree.
+			 * Initialize a MoveTree without coordinates. Primarily used for the
+			 * root of the tree.
 			 */
+			for ( Integer x: coords) 
+				x = null;
+			this.player = player;
 		}
 
-		public MoveTree(int x, int y, int value) {
+		public MoveTree(int player, int x, int y, int z) {
 			/*
-			 * Initialize a MoveTree with coordinates.
-			 * Use this one unless you know what you're doing!
+			 * Initialize a MoveTree with coordinates. Use this one unless you
+			 * know what you're doing!
 			 */
+			this.player = player;
 			coords[0] = x;
 			coords[1] = y;
-			coords[2] = value;
+			coords[2] = z;
+		}
+
+		public double getHeuristic() {
+			if (heuristicValue == null) {
+				Double heuristic = null;
+				if (possibleMoves != null) {
+					for (MoveTree move : possibleMoves) {
+						if (heuristic == null || heuristic < move.getHeuristic())
+							heuristic = move.getHeuristic();
+					}
+				} else {
+					int row = coords[0];
+					int column = coords[1];
+					int height = coords[3];
+					Integer z = null;
+					for (int i = board.length - 1; i >= 0; i--) {
+						if (board[i][column][row] != 0) {
+							z = i;
+							break;
+						}
+					}
+					int[] temp = {0, 1};
+					for (int xAxis : temp)
+						for (int yAxis : temp)
+							for (int zAxis : temp) {
+
+								if (xAxis == 0 && yAxis == 0 && zAxis == 0) ;
+								else {
+									int count = 1;
+									int x = row;
+									int y = column;
+									z = height;
+									checkForward:
+									try {
+										while (board[z + zAxis][y + yAxis][x + xAxis] == board[z][y][x]) {
+											count++;
+											z += zAxis;
+											y += yAxis;
+											x += xAxis;
+										}
+									} catch (ArrayIndexOutOfBoundsException e) {
+										int ztemp = z;
+										int ytemp = y;
+										int xtemp = x;
+										if (z + zAxis >= board.length)
+											z = -1;
+										if (y + yAxis >= board[z].length)
+											y = -1;
+										if (x + xAxis >= board[z][column].length)
+											x = -1;
+										if (board[ztemp][ytemp][xtemp] == board[z + zAxis][y + yAxis][x + xAxis]) {
+											count++;
+											z += zAxis;
+											y += yAxis;
+											x += xAxis;
+											break checkForward;
+										}
+									}
+									x = row;
+									y = column;
+									z = height;
+									checkBackward:
+									try {
+										while (board[z - zAxis][y - yAxis][x - xAxis] == board[z][y][x]) {
+											count++;
+											z -= zAxis;
+											y -= yAxis;
+											x -= xAxis;
+										}
+									} catch (ArrayIndexOutOfBoundsException e) {
+										int ztemp = z;
+										int ytemp = y;
+										int xtemp = x;
+										if (z - zAxis < 0)
+											z = board.length;
+										if (y - yAxis < 0)
+											y = board[z].length;
+										if (x - xAxis < 0)
+											x = board[z][column].length;
+										if (board[ztemp][ytemp][xtemp] == board[z - zAxis][y - yAxis][x - xAxis]) {
+											count++;
+											z -= zAxis;
+											y -= yAxis;
+											x -= xAxis;
+											break checkBackward;
+										}
+									}
+									heuristic = Math.pow(2, count);
+								}
+							}
+				}
+				heuristicValue = heuristic;
+				return heuristic;
+			} else
+				return heuristicValue;
+		}
+
+		public void prune() {
+			aiThread.interrupt();
+			Double bestHeuristic = null;
+			for (MoveTree move : possibleMoves) {
+				double moveHeuristic = move.getHeuristic();
+				if (bestHeuristic == null || bestHeuristic > moveHeuristic) {
+					possibleMoves.removeElement(move);
+				} else if (bestHeuristic < moveHeuristic)
+					bestHeuristic = moveHeuristic;
+			}
 		}
 
 		public boolean populate() throws InterruptedException {
-			possibleMoves = new Vector<MoveTree>(); // Empty the possible moves to make sure there are no repeats.
+			heuristicValue = null;
+			possibleMoves = new Vector<MoveTree>(); // Empty the possible moves
+													// to make sure there are no
+													// repeats.
+			int tempPlayer = player + 1;
+			if (tempPlayer > playerCount)
+				tempPlayer = 1;
+
 			for (int x = 0; x < board[0][0].length; x++)
 				for (int y = 0; y < board[0].length; y++) {
 					int z = 0;
 					try {
-						while (board[z][y][x] != 0) // Try to find the vertical zone to place the piece in so we can make sure the move is valid.
+						while (board[z][y][x] != 0) // Try to find the vertical
+													// zone to place the piece
+													// in so we can make sure
+													// the move is valid.
 							z++;
 						if (Thread.interrupted()) {
 							InterruptedException e = new InterruptedException();
 							throw e;
 						}
-						// Add code to filter results here.
+						MoveTree newMove = new MoveTree(tempPlayer, x, y, z); // Filter
+																				// possible
+																				// moves
+						if ( (board.length <= 6 && board[0].length <= 6 && board[0][0].length <= 6)
+								|| board.length < 5 || board[0].length < 5 || board[0][0].length < 5)
+							possibleMoves.add(newMove);
+						else
+						try {
+							if (z != 0 || board[0][y + 1][x] != 0 || board[0][y][x + 1] != 0
+									|| board[0][y + 1][x + 1] != 0 || board[0][y - 1][x] != 0 || board[0][y][x - 1] != 0
+									|| board[0][y - 1][x - 1] != 0 || board[0][x - 1][y + 1] != 0
+									|| board[0][x + 1][y - 1] != 0)
+								possibleMoves.add(newMove);
+						} catch (ArrayIndexOutOfBoundsException e) {
+
+						}
 					} catch (ArrayIndexOutOfBoundsException e) {
 
 					}
 				}
-			if (possibleMoves.isEmpty())
+			if (possibleMoves.isEmpty()){
+				if ( coords[0] == null){
+					possibleMoves.addElement(new MoveTree(tempPlayer,2,2,0));
+					return true;
+				}else
+					for ( int y = 0; y < board[0].length; y++)
+						for ( int x = 0; x < board[0][y].length; x++){
+							if (board[0][y][x] != 0){
+								int zz = 0;
+								for( int z = 0; z < board.length; z++)
+									if ( board[z][y][x] == 0) zz = z;
+								if (zz != 0){
+									possibleMoves.add(new MoveTree(tempPlayer, x, y , zz));
+									return true;
+								}
+							}
+						}
 				return false;
+			}
 			else
 				return true;
 		}
-		/*public int heuristic() {
-			//initialize a heuristic tree, 2 dimensional; one is a move array, the other is a value
-			  while possibleMoves != null {
-			 if possibleMoves.nextnode == (piece) {
-			  (piece)count++;
-			  (piece)value = 2^count;
-			  heuristic();
-			  }
-			  if ((piece = playerPiece +1)value >= 8)   {
-			  	move to block that player;
-			  else {
-			  	make a move to highest (playerPiece)value;
-			  }
-			 */
 
 		public void populateAll() throws InterruptedException {
 			if (populate())
 				for (MoveTree move : possibleMoves) {
 					move.populateAll();
-					//move.prune();
+					move.prune();
 				}
 		}
 	}
--- Connect5/src/GUI.java
+++ Connect5/src/GUI.java
@@ -84,6 +84,8 @@ public class GUI  extends JFrame {
         JPanel gameWindow = new JPanel(new BorderLayout());
         private BoardPanel boardPanel = new BoardPanel();
         JLabel statusLbl, playerLbl;
+        JSpinner xSpinner, ySpinner;
+        JButton commitBtn;
 
 
         public GameView() {
@@ -94,6 +96,7 @@ public class GUI  extends JFrame {
             JButton startbtn = new JButton("Start");
             JButton resetBtn = new JButton("Reset");
             resetBtn.addActionListener(new ResetButton());
+            startbtn.addActionListener(new StartButton());
             JPanel subPanel1 = new JPanel();
             subPanel1.add(startbtn);
             subPanel1.add(resetBtn);
@@ -102,10 +105,12 @@ public class GUI  extends JFrame {
             JPanel subPanel2 = new JPanel();
             gameWindow.add(subPanel2, BorderLayout.CENTER);
 
-            xCountModel = new SpinnerNumberModel(0, 0, (int) widthModel.getNumber(), 1);
-            yCountModel = new SpinnerNumberModel(0, 0, (int) lengthModel.getNumber(), 1);
-            JSpinner xSpinner = new JSpinner(xCountModel);
-            JSpinner ySpinner = new JSpinner(yCountModel);
+            xCountModel = new SpinnerNumberModel(0, 0, (int) widthModel.getNumber()-1, 1);
+            yCountModel = new SpinnerNumberModel(0, 0, (int) lengthModel.getNumber()-1, 1);
+            xSpinner = new JSpinner(xCountModel);
+            ySpinner = new JSpinner(yCountModel);
+            xSpinner.setPreferredSize(new Dimension(50,25));
+            ySpinner.setPreferredSize(new Dimension(50,25));
             JLabel xlbl = new JLabel("x");
             JLabel ylbl = new JLabel("y");
             JPanel subPanel3 = new JPanel();
@@ -114,7 +119,7 @@ public class GUI  extends JFrame {
             subPanel3.add(ylbl);
             subPanel3.add(ySpinner);
 
-            JButton commitBtn = new JButton("Commit Move");
+            commitBtn = new JButton("Commit Move");
             commitBtn.addActionListener(new commitMove());
             Box subPanel4 = new Box(BoxLayout.PAGE_AXIS);
             subPanel4.add(subPanel3);
@@ -127,6 +132,7 @@ public class GUI  extends JFrame {
             playerLbl.setFont(new Font("Default", Font.BOLD, 20));
             JPanel subPanel5 = new JPanel();
             subPanel5.add(playerLbl);
+            subPanel5.add(Box.createRigidArea(new Dimension(200, 0)));
             subPanel5.add(statusLbl);
             gameWindow.add(subPanel5, BorderLayout.PAGE_END);
 
@@ -136,6 +142,9 @@ public class GUI  extends JFrame {
             boardPanel.setBackground(Color.CYAN);
             gameWindow.add(boardPanel, BorderLayout.CENTER);
 
+            xSpinner.setEnabled(false);
+            ySpinner.setEnabled(false);
+            commitBtn.setEnabled(false);
 
             setVisible(true);
         }
@@ -146,81 +155,89 @@ public class GUI  extends JFrame {
                 int z = (int) heightModel.getNumber();
                 int y = (int) widthModel.getNumber();
                 int x = (int) lengthModel.getNumber();
-                int xVal = 15*x-15;
-                int yVal = (15*y*z)+(15*(z-2));
                 int counter = 1;
+                int scaleFactor = 0;
+                int n = x + y + z;
+
+                if (n<=30) scaleFactor = 15;
+                if (n<=20) scaleFactor = 20;
+                if (n<=10) scaleFactor = 30;
+
+
+                int xVal = scaleFactor*x-scaleFactor;
+                int yVal = (scaleFactor*y*z)+(scaleFactor*(z-1));
 
                 if (z>5) {
-                    xVal = (15*x*2)+15; //we need 2 per row b/c too tall
-                    if (z%2 != 0) yVal = yVal/2 + 30;
+                    xVal = (scaleFactor*x*2)+scaleFactor; //we need 2 per row b/c too tall
+                    if (z%2 != 0) yVal = yVal/2 + (2*scaleFactor);
                     else yVal = yVal/2;
                 }
                 for (int i = 0; i < z; i++) {
                     for (int j = 0; j < y; j++) {
                         for (int k = 0; k < x; k++) {
                             g.setColor(Color.BLACK);
-                            g.drawRect(xVal, yVal, 15, 15);
+                            g.drawRect(xVal, yVal, scaleFactor, scaleFactor);
                             switch (board.getBoard()[i][j][k]){
                                 case 1: {
                                     g.setColor(Color.RED);
-                                    g.fillRect(xVal, yVal, 15, 15);
+                                    g.fillRect(xVal, yVal, scaleFactor, scaleFactor);
                                     break;
                                 }
                                 case 2: {
                                     g.setColor(Color.GREEN);
-                                    g.fillRect(xVal, yVal, 15, 15);
+                                    g.fillRect(xVal, yVal, scaleFactor, scaleFactor);
                                     break;
                                 }
                                 case 3: {
                                     g.setColor(Color.BLUE);
-                                    g.fillRect(xVal, yVal, 15, 15);
+                                    g.fillRect(xVal, yVal, scaleFactor, scaleFactor);
                                     break;
                                 }
                                 case 4: {
                                     g.setColor(Color.YELLOW);
-                                    g.fillRect(xVal, yVal, 15, 15);
+                                    g.fillRect(xVal, yVal, scaleFactor, scaleFactor);
                                     break;
                                 }
                                 case 5: {
                                     g.setColor(Color.ORANGE);
-                                    g.fillRect(xVal, yVal, 15, 15);
+                                    g.fillRect(xVal, yVal, scaleFactor, scaleFactor);
                                     break;
                                 }
                             }
                             try {
                                 for (int l = 0; l < 5; l++) {
-                                    for (int m = 0; m < 1; m++) {
-                                        if (k == board.getWinTiles()[l][m] && j == board.getWinTiles()[l][m + 1]) {
-                                            g.setColor(Color.MAGENTA);
-                                            g.fillRect(xVal, yVal, 15, 15);
-                                        }
+                                    if (k == board.getWinTiles()[l][0] && j == board.getWinTiles()[l][1] && i == board.getWinTiles()[l][2]) {
+                                        g.setColor(Color.MAGENTA);
+                                        g.fillRect(xVal, yVal, scaleFactor, scaleFactor);
                                     }
                                 }
                             }
                             catch (Exception e){}
 
-                            xVal = xVal - 15;
+                            xVal = xVal - scaleFactor;
                         }
-                        yVal = yVal - 15;
-                        if (z<=5) xVal = 15*x-15;
+                        yVal = yVal - scaleFactor;
+                        if (z<=5) xVal = scaleFactor*x-scaleFactor;
                         else {
-                            if (counter != 2) xVal = (15*x*2)+15;
-                            else xVal = (xVal + (15*x));
+                            if (counter != 2) xVal = (scaleFactor*x*2)+scaleFactor;
+                            else xVal = (xVal + (scaleFactor*x));
                         }
                     }
-                    if (z<=5) yVal = yVal - 15;
+                    if (z>5) g.drawString("Floor: " + i,xVal-scaleFactor,yVal+scaleFactor);
+                    if (z<=5) yVal = yVal - scaleFactor;
                     else {
                         if (counter != 2) {
-                            yVal = yVal + (15*y);
-                            xVal = (xVal - (15*x)) - 15;
+                            yVal = yVal + (scaleFactor*y);
+                            xVal = (xVal - (scaleFactor*x)) - scaleFactor;
                         }
                         else {
-                            xVal = (15*x*2)+15;
-                            yVal = yVal-15;
+                            xVal = (scaleFactor*x*2)+scaleFactor;
+                            yVal = yVal-scaleFactor;
                         }
                     }
                     counter++;
                     if (counter > 2) counter = 1;
+                    if (z<=5) g.drawString("Floor: " + i,xVal-scaleFactor,yVal+(2*scaleFactor));
                 }
             }
         }
@@ -234,16 +251,31 @@ public class GUI  extends JFrame {
             }
         }
 
+        private class StartButton implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                xSpinner.setEnabled(true);
+                ySpinner.setEnabled(true);
+                commitBtn.setEnabled(true);
+            }
+        }
+
         private class commitMove implements ActionListener {
+            int winner;
             @Override
             public void actionPerformed(ActionEvent arg0) {
                 try {
                     if (!board.getCurrentPlayer().move((int) xCountModel.getNumber(), (int) yCountModel.getNumber())) {
-                        playerLbl.setText("It's " + board.getCurrentPlayer() + "'s turn");
+                        playerLbl.setText("It's " + board.getCurrentPlayer().playerNumber + "'s turn");
+                        statusLbl.setText("Status: Game in Progress");
+                        winner = board.getCurrentPlayer().playerNumber;
                     }
                     else{
-                        playerLbl.setText("WINNER: " + board.getCurrentPlayer());
-                        statusLbl.setText("GAME OVER");
+                        playerLbl.setText("WINNER: Player " + winner);
+                        statusLbl.setText("Status: GAME OVER");
+                        xSpinner.setEnabled(false);
+                        ySpinner.setEnabled(false);
+                        commitBtn.setEnabled(false);
                     }
                 }
                 catch (Exception e){
